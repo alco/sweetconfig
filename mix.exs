c7ddc2451669888a160d1f2f22c295eb4de32187
Alexei Sholik <alcosholik@gmail.com>
2014-08-12 16:03:25 +0300

    Bump version and support Elixir v0.14 and above

--- mix.exs
+++ mix.exs
@@ -3,8 +3,8 @@ defmodule Sweetconfig.Mixfile do
 
   def project do
     [app: :sweetconfig,
-     version: "0.0.1",
-     elixir: "~> 0.14.0",
+     version: "0.5.0-dev",
+     elixir: "~> 0.14",
      deps: deps]
   end
 
